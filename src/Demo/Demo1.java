76d787dd354f117cab698b071d1e507757fbe6de
saicharanmarla <saicharanmarla75@gmail.com>
2025-11-14 19:52:47 +0530

    Added Demo2 File

--- src/Demo/Demo1.java
+++ src/Demo/Demo1.java
@@ -5,6 +5,12 @@ public class Demo1 {
 	public static void main(String[] args)
 	{
 		System.out.println("Demo File Eclipse");
+		funA();
+	}
+	
+	public static void funA()
+	{
+		System.out.println("funA");
 	}
 
 }
